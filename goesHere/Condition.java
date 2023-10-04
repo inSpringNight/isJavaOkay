140076cd2310a52e9e72771a6f22e237ef290c57
BuildTools <unconfigured@null.spigotmc.org>
2023-10-04 21:58:22 +0700

    add 1 file about IF condition

---
+++ goesHere/Condition.java
@@ -0,0 +1,15 @@
+package goesHere;
+
+public class Condition {
+    public static void main(String[] args) {
+        System.out.println("Hello, SummerNight");
+
+        boolean isHuman = true;
+        if (isHuman == false) {
+            System.out.println("You're not a Human");
+            System.out.println("Go Away!");
+        } else {
+            System.out.println("Welcome, Human. We come in peace");
+        }
+    }
+}
