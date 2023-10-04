5dcc3a72738fc70b7ea5723163763082e4a8fe20
BuildTools <unconfigured@null.spigotmc.org>
2023-10-04 21:47:59 +0700

    add 2 new file for same print, and introducing println

---
+++ goesHere/Hello2.java
@@ -0,0 +1,8 @@
+package goesHere;
+
+public class Hello2 {
+    public static void main(String[] args){
+        System.out.println("This is");
+        System.out.print("Example of Hello print");
+    }
+}
---
+++ goesHere/SecondClass.java
@@ -0,0 +1,7 @@
+package goesHere;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.print("Hello, SummerNight");
+    }
+}
